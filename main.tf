f2e53e3d080bcfb547446b2ea61f13a2eac07ab7
pacoard <pacoard@gmail.com>
2023-03-13 19:17:19 -0700

    progress

---
+++ main.tf
@@ -0,0 +1,10 @@
+## apigateway api
+## apigateway resource
+## lambda function
+## lambda-resource association
+## terraform output endpoint of resource
+
+# Mix these tutorials:
+# - https://medium.com/@haissamhammoudfawaz/create-a-aws-lambda-function-using-terraform-and-python-4e0c2816753a
+# - https://spacelift.io/blog/terraform-aws-lambda#1-set-up-the-iam-roles-and-policies
+# - https://callaway.dev/deploy-python-lambdas-with-terraform/
